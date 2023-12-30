0ed35694ee84828d42acc63eadee8ab82ddfbced
kangminseung <whelming25@naver.com>
2023-12-30 13:36:17 +0900

    feat: v1 개발

--- src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.hellotrace.HelloTraceV1;
+import hello.advanced.trace.TraceStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +9,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderServiceV1 {
 
+
     private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
 
     public void orderItem(String itemId) {
-        orderRepository.save(itemId);
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
     }
-
 }
