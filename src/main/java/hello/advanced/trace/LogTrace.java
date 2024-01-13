c5a73752577484ec27c3eb3a6e2d3aa593704fa0
kangminseung <msk109@inaus.co.kr>
2024-01-13 19:03:22 +0900

    feat: 쓰레드 로컬

---
+++ src/main/java/hello/advanced/trace/LogTrace.java
@@ -0,0 +1,7 @@
+package hello.advanced.trace;
+
+public interface LogTrace {
+     TraceStatus begin(String message);
+     void end(TraceStatus status);
+     void exception(TraceStatus status, Exception e);
+}
