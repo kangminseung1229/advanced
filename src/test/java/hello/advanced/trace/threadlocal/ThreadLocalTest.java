c6593651f31879d56a00f33c4b6b225890604cf1
kangminseung <msk109@inaus.co.kr>
2024-02-11 16:15:12 +0900

    feat: 스레드로컬서비스 테스트 완료

--- src/test/java/hello/advanced/trace/threadlocal/ThreadLocalTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalTest.java
@@ -1,37 +1,32 @@
 package hello.advanced.trace.threadlocal;
 
-import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-public class FieldServiceTest {
+public class ThreadLocalTest {
 
-    private FieldService fieldService = new FieldService();
+    private ThreadLocalService service = new ThreadLocalService();
 
     @Test
-    void field() {
+    void threadLocal() {
         log.info("main start");
         Runnable userA = () -> {
-            fieldService.logic("userA");
+            service.logic("userA");
         };
-
         Runnable userB = () -> {
-            fieldService.logic("userB");
+            service.logic("userB");
         };
-
         Thread threadA = new Thread(userA);
         threadA.setName("thread-A");
         Thread threadB = new Thread(userB);
         threadB.setName("thread-B");
-
         threadA.start();
         sleep(100);
         threadB.start();
-
-//        sleep(3000);
+        sleep(2000);
         log.info("main exit");
-
     }
 
     private void sleep(int millis) {
@@ -41,5 +36,4 @@ public class FieldServiceTest {
             e.printStackTrace();
         }
     }
-
 }
